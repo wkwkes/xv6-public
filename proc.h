177e21a44ffb52955535224f8bffa2aff6ebbe05
wkwkes <wkwkes@gmail.com>
2017-06-29 20:23:23 +0900

    change uint to int

--- proc.h
+++ proc.h
@@ -64,7 +64,7 @@ struct proc {
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
 
-  uint pri;                    // Process priority, <= 10
+  int pri;                    // Process priority, <= 10
 };
 
 // Process memory is laid out contiguously, low addresses first:
