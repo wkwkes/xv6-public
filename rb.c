808cd7c8da9c2779d97f4836c7c2ce616569d0ad
wkwkes <wkwkes@gmail.com>
2017-07-03 00:41:13 +0900

    add delete partially

--- rb.c
+++ rb.c
@@ -30,6 +30,13 @@ struct node
 };
 
 struct node* groot;
+void
+groot_mod(struct node* nd)
+{
+  while (nd->parent)
+    nd = nd->parent;
+  groot = nd;
+}
 
 void
 node_init(struct node* nd,
@@ -98,6 +105,7 @@ dump_nodes(struct node* root, int indent)
   if (root->left != NULL) {
     dump_nodes(root->left, indent + 5);
   }
+
   if (root->right != NULL) {
     dump_nodes(root->right, indent + 5);
   }
@@ -133,6 +141,7 @@ insert_helper(struct node* root, struct node* nd)
     if (root->left == NULL) {
       root->left = nd;
       nd->parent = root;
+      groot_mod(root);
     } else {
       insert_helper(root->left, nd);
     }
@@ -141,6 +150,7 @@ insert_helper(struct node* root, struct node* nd)
     if (root->right == NULL) {
       root->right = nd;
       nd->parent = root;
+      groot_mod(root);
     } else {
       insert_helper(root->right, nd);
     }
@@ -164,9 +174,6 @@ rotate_right(struct node* c, struct node* p)
   }
   p->parent = c;
   p->left = sub;
-
-  //   printf("ratate right begins\n");
-  //   return;
 
   if (sub)
     sub->dir = 0;
@@ -202,11 +209,11 @@ insert(struct node* root, struct node* nd)
   nd->color = RED;
 
   insert_helper(root, nd);
-  //   printf("after BST insert\n\n");
-  //   dump_nodes(root, 0);
-  //   printf(" - - - - - - - - \n\n\n");
 
-  // recoloring and rotation
+  // printf("\n");
+  // dump_nodes(groot, 10);
+  // printf("\n");
+
   while (1) {
     if (nd->parent == NULL) {
       nd->color = BLACK;
@@ -238,7 +245,7 @@ insert(struct node* root, struct node* nd)
 
       gp->color = RED;
 
-      swap(nd, gp);
+      // swap(nd, gp);
       nd = gp;
       continue;
     } else {                               // rotation
@@ -265,97 +272,63 @@ insert(struct node* root, struct node* nd)
     }
   }
   // find root
-  struct node* tmp = nd;
-  while (1) {
-    if (tmp->parent == NULL) {
-      groot = tmp;
-      break;
-    } else {
-      tmp = tmp->parent;
-    }
-  }
+  groot_mod(nd);
 }
 
-void
-insert_debug(struct node* root, struct node* nd)
+void delete (struct node* root, int proc_index)
 {
-  nd->color = RED;
-
-  insert_helper(root, nd);
-  printf("after BST insert\n\n");
-  dump_nodes(root, 0);
-  printf(" - - - - - - - - \n\n\n");
-
-  // recoloring and rotation
-  while (1) {
-    if (nd->parent == NULL) {
-      nd->color = BLACK;
+  struct node* u = NULL; // child that replace v
+  struct node* v = NULL; // to delete
+  struct node* root_ = root;
+  while (u == NULL && v == NULL) {
+    if (root_ == NULL) {
       break;
     }
-
-    struct node* par = nd->parent;
-    if (par->color == BLACK) {
-      break;
-    }
-
-    struct node* gp = par->parent;
-
-    // if(gp == NULL) {
-    //     printf("gp is NULL!\n");
-    // } else {
-    //     printf("gp is not NULL!\n");
-    // }
-    // return;
-
-    struct node* uncle;
-    if (par->dir) { // right
-      uncle = gp->left;
-    } else {
-      uncle = gp->right;
-    }
-
-    ///////////////////////////////////////////
-    // 親を変える時, 親の子を指すポインタも変える！！！
-    ///////////////////////////////////////////
-    enum Color uncle_color;
-    if (uncle == NULL) {
-      uncle_color = BLACK;
-    } else {
-      uncle_color = uncle->color;
-    }
-
-    if (uncle_color == RED) {
-      par->color = BLACK;
-      uncle->color = BLACK;
-
-      gp->color = RED;
-
-      swap(nd, gp);
-      nd = gp;
-      continue;
-    } else {                               // rotation
-      if (par->dir == 0 && nd->dir == 0) { // left-left
-        par->color = BLACK;
-        gp->color = RED;
-        rotate_right(par, gp);
-      } else if (par->dir == 0 && nd->dir == 1) { // left-right
-        nd->color = BLACK;
-        gp->color = RED;
-        rotate_left(nd, par);
-        rotate_right(nd, gp);
-      } else if (par->dir == 1 && nd->dir == 0) { // right-left
-        nd->color = BLACK;
-        gp->color = RED;
-        rotate_right(nd, par);
-        rotate_left(nd, gp);
-      } else if (par->dir == 1 && nd->dir == 1) { // right-right
-        par->color = BLACK;
-        gp->color = RED;
-        rotate_left(nd, par);
+    printf("here!!\n");
+    if (root_->proc_index == proc_index) { // delete root
+      printf("here!!\n");
+      if (root_->left != NULL && root_->right != NULL) {
+        struct node* min_node = root_->right;
+        while (min_node->left != NULL) {
+          min_node = min_node->left;
+        }
+        printf("here!!\n");
+        swap(min_node, root_);
+        root_ = min_node;
+        proc_index = min_node->proc_index;
+      } else if (root_->left != NULL) {
+        u = root_->left;
+        v = root_;
+      } else if (root_->right != NULL) {
+        u = root_->right;
+        v = root_;
+      } else {
+        v = root_;
       }
-      break;
+    } else if (proc_index < root_->proc_index) {
+      printf("here!ll(%d, %d)\n", proc_index, root_->proc_index);
+      root_ = root_->left;
+    } else { // proc_index > root_->proc_index
+      printf("here!rr(%d, %d)\n", proc_index, root_->proc_index);
+      root_ = root_->right;
     }
   }
+
+  if (u) {
+    printf("\nu\n");
+    dump_node(u, 10);
+    groot_mod(u);
+  } else {
+    printf("u is NULL\n");
+  }
+  if (v) {
+    printf("\nv\n");
+    dump_node(v, 10);
+    groot_mod(v);
+  } else {
+    printf("v is NULL\n");
+  }
+  dump_nodes(groot, 0);
 }
 
 int
@@ -373,29 +346,31 @@ main()
   node_init(ll, 700, RED, 3, 0, NULL, NULL, NULL);
   node_init(ins, 850, RED, 4, 0, NULL, NULL, NULL);
 
-  //   node_init(nd, 1000, BLACK, 0, 0, NULL, l, r);
-  //   node_init(l, 900, BLACK, 1, 0, nd, ll, NULL);
-  //   node_init(r, 800, BLACK, 2, 1, nd, NULL, NULL);
-  //   node_init(ll, 700, RED, 3, 0, l, NULL, NULL);
-  //   node_init(ins, 850, RED, 4, 0, NULL, NULL, NULL);
+  // node_init(nd, 1000, BLACK, 0, 0, NULL, l, r);
+  // node_init(l, 900, BLACK, 1, 0, nd, ll, NULL);
+  // node_init(r, 800, BLACK, 2, 1, nd, NULL, NULL);
+  // node_init(ll, 700, RED, 3, 0, l, NULL, NULL);
+  // node_init(ins, 850, RED, 4, 0, NULL, NULL, NULL);
 
   dump_nodes(nd, 0);
-
+  groot = nd;
   printf("\n\n\n");
-  insert(nd, l);
+  insert(groot, l);
   dump_nodes(groot, 0);
 
   printf("\n\n\n");
-  insert(nd, r);
+  insert(groot, r);
   dump_nodes(groot, 0);
 
   printf("\n\n\n");
-  insert(nd, ll);
+  insert(groot, ll);
   dump_nodes(groot, 0);
 
   printf("\n\n\n");
-  insert(nd, ins);
+  insert(groot, ins);
   dump_nodes(groot, 0);
+
+  delete (groot, 2);
 
   free(nd);
   free(l);
